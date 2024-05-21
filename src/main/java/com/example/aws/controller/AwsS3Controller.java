4a8abb32dbebbc700fd05f5f8ef09410bbc7b07c
chuman <chuman.panda@gmail.com>
2024-05-21 17:50:09 +0530

    added Cross Origin fix for UI

--- src/main/java/com/example/aws/controller/AwsS3Controller.java
+++ src/main/java/com/example/aws/controller/AwsS3Controller.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import com.example.aws.service.impl.S3ServiceImpl;
 
 import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/files")
 public class AwsS3Controller {
